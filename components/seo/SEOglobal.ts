a7d3d8b00f1de0a09eb4b6d459e10698511c283b
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2022-12-09 11:02:05 +0800

    initial commit

---
+++ components/seo/SEOglobal.ts
@@ -0,0 +1,10 @@
+const globalMeta = {
+  siteName: "Regres - NextJS TSX Template",
+  siteUrl: "https://yoursite.com",
+  canonicalUrl: "https://yoursite.com",
+  siteLogo: "https://yoursite.com/images/logo.png",
+  email: "email@yoursite.com",
+  description: "Default site description.",
+};
+
+export default globalMeta;
